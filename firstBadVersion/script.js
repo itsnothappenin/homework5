7a04ae9ed30822345fcccc92afa6900a393f2525
itsnothappenin <105514163+itsnothappenin@users.noreply.github.com>
2022-08-05 15:59:35 +0400

    Add files via upload

---
+++ firstBadVersion/script.js
@@ -0,0 +1,16 @@
+var solution = function(isBadVersion) {
+
+    return function(n) {
+          let left = 1, right = n
+        while(left < right) {
+            const mid = Math.floor((right + left) / 2)
+            if(isBadVersion(mid)) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return right
+
+    };
+};
